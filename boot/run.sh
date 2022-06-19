3286ad678c4e5c78532ab23ae7ad5c7afe21923f
kolyasuk <dzioba99@gmail.com>
2022-06-19 13:02:25 +0300

    run.sh

--- boot/run.sh
+++ boot/run.sh
@@ -1,2 +1,2 @@
-docker stop cms-api || true && docker rm cms-api || true
+docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null
 docker run --name cms-api -d -p 8080:8080 dzioba/cms-api
