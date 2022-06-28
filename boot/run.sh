b5cbb11f66ac7a539775d3c072cd10e911532581
kolyasuk <dzioba99@gmail.com>
2022-06-28 22:18:54 +0300

    Add echo

--- boot/run.sh
+++ boot/run.sh
@@ -1,3 +1,4 @@
 # $1 environment
-docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null
-docker run --name cms-api -d -e env=$1 -p 8080:8080 dzioba/cms-api
+echo "Running environment: $1";
+docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null;
+docker run --name cms-api -d -e env=$1 -p 8080:8080 dzioba/cms-api;
