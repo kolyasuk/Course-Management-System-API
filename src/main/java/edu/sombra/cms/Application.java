99e8d9c4f69ebdced0d85468e12a5b4988cfc5c2
kolyasuk <dzioba99@gmail.com>
2022-06-29 00:45:54 +0300

    Fix

--- src/main/java/edu/sombra/cms/Application.java
+++ src/main/java/edu/sombra/cms/Application.java
@@ -2,12 +2,17 @@ package edu.sombra.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@PropertySources({
+        @PropertySource("classpath:app_config/application.properties"),
+        @PropertySource("classpath:app_config/application-${spring.profiles.active}.properties")
+})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
