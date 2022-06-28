99f9d040a9a0ea109397136ed1249aa395133b65
kolyasuk <dzioba99@gmail.com>
2022-06-28 23:50:25 +0300

    Fix

--- boot/run.sh
+++ boot/run.sh
@@ -1,4 +1,4 @@
 # $1 environment
 echo "Running environment: $1";
 docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null;
-docker run --name cms-api -d -e env=$1 -p 8080:8080 dzioba/cms-api;
+docker run --name cms-api -d -e App.config.file=/home/dzioba99/cms-api/config/application-$1.properties env=$1 -p 8080:8080 dzioba/cms-api;
