c27f76f86f5ed5c04c24989e44d7525a7d87fbb1
kolyasuk <dzioba99@gmail.com>
2022-07-09 19:40:49 +0300

    final fix

--- boot/run-test.sh
+++ boot/run-test.sh
@@ -3,5 +3,4 @@
 # $3 p
 echo "Running environment: $1";
 docker login -u $2 -p $3
-sudo docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null;
-sudo docker run --pull=always --restart always --name cms-api -d -e spring.profiles.active=$1 -p 8080:8080 dzioba/cms-api
+PROFILE_NAME=test docker compose up -d
