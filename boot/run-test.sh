953835544f0819f66314b9bb80ac2743a64e7c5c
kolyasuk <dzioba99@gmail.com>
2022-07-09 20:16:31 +0300

    final fix

--- boot/run-test.sh
+++ boot/run-test.sh
@@ -3,5 +3,5 @@
 # $3 p
 echo "Running environment: $1";
 docker login -u $2 -p $3
-sudo docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null;
+docker compose down
 PROFILE_NAME=$1 docker compose up -d
