8b02e4d48ec5b5112180f3c28f8082e2fd3e5268
kolyasuk <dzioba99@gmail.com>
2022-07-31 15:14:01 +0300

    Fix run.sh

--- boot/run.sh
+++ boot/run.sh
@@ -4,4 +4,4 @@
 echo "Running environment: $1";
 docker login -u $2 -p $3
 sudo docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null;
-sudo docker run --pull=always --restart always --name cms-api -d -e spring.profiles.active=$1 -p 8080:8080 dzioba/cms-api
+sudo docker run --pull=always --restart always --name cms-api -d -e SPRING_PROFILE=$1 -p 8080:8080 dzioba/cms-api
