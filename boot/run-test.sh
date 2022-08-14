41c4e3bf78ab5b03537ded30763aac075635a9b7
kolyasuk <dzioba99@gmail.com>
2022-08-14 14:14:34 +0300

    Fix

--- boot/run-test.sh
+++ boot/run-test.sh
@@ -5,6 +5,7 @@ echo "Running environment: $1";
 pwd
 docker login -u $2 -p $3
 sudo docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null
+sudo docker stop jenkins-cms-api-1  2> /dev/null && docker rm jenkins-cms-api-1  2> /dev/null;
 docker compose down
 docker compose pull
 PROFILE_NAME=$1 docker compose up -d
--- boot/run.sh
+++ boot/run.sh
@@ -4,4 +4,5 @@
 echo "Running environment: $1";
 docker login -u $2 -p $3
 sudo docker stop cms-api  2> /dev/null && docker rm cms-api  2> /dev/null;
+sudo docker stop jenkins-cms-api-1  2> /dev/null && docker rm jenkins-cms-api-1  2> /dev/null;
 sudo docker run --pull=always --restart always --name cms-api -d -e SPRING_PROFILE=$1 -p 8080:8080 dzioba/cms-api
